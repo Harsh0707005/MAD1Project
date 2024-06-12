ed169d3faed8228404ed7556505fbe0740c4c1a7
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-12 23:23:19 +0530

    Login Logic

--- app.py
+++ app.py
@@ -10,7 +10,28 @@ connect.execute('CREATE TABLE IF NOT EXISTS users (email TEXT NOT NULL PRIMARY K
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        pass
+        email = request.form['email'].strip()
+        password = request.form['password'].strip()
+
+        if email == "" or password == "":
+            return render_template('login.html', errMessage = "Please fill in all fields!", emailInput = email)
+
+        with sqlite3.connect('users.db') as users:
+            cursor = users.cursor()
+
+            cursor.execute('SELECT * FROM users where email = ?', (email,))
+
+            db_res = cursor.fetchone()
+
+            if db_res:
+                if password == db_res[1]:
+                    return render_template('login.html', errMessage = "Login Successful!")
+                else:
+                    return render_template('login.html', emailInput = email, errMessage = "Invalid Password!")
+                
+            else:
+                return render_template('login.html', errMessage = "User doesn't exist!")
+
     return render_template("login.html")
 
 @app.route("/register/sponsor", methods=["GET", "POST"])
@@ -18,9 +39,12 @@ def registerSponsor():
     if request.method == "POST":
 
         role = "sponsor"
-        email = request.form["email"]
-        password = request.form["password"]
+        email = request.form["email"].strip()
+        password = request.form["password"].strip()
         confirm_password = request.form["confirm_password"]
+
+        if email == "" or password == "":
+            return render_template('register.html', errMessage = "Please fill in all fields!", emailInput = email)
 
         if password != confirm_password:
             return render_template('register.html', errMessage = "Passwords do not match!", emailInput = email)
@@ -44,9 +68,12 @@ def registerSponsor():
 def registerInfluencer():
     if request.method == "POST":
         role = "influencer"
-        email = request.form["email"]
-        password = request.form["password"]
-        confirm_password = request.form["confirm_password"]
+        email = request.form["email"].strip()
+        password = request.form["password"].strip()
+        confirm_password = request.form["confirm_password"].strip()
+
+        if email == "" or password == "" or confirm_password == "":
+            return render_template('register.html', errMessage = "Please fill in all fields!", emailInput = email)
 
         if password != confirm_password:
             return render_template('register.html', errMessage = "Passwords do not match!", emailInput = email)
