261159243c0d1c1845efc08efae3d066d9f17155
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-12 20:22:38 +0530

    Registration

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, url_for
+import sqlite3
 
 app = Flask(__name__)
+
+connect = sqlite3.connect('users.db')
+connect.execute('CREATE TABLE IF NOT EXISTS users (email TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL, role TEXT NOT NULL)')
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/login", methods=["GET", "POST"])
@@ -12,14 +16,52 @@ def login():
 @app.route("/register/sponsor", methods=["GET", "POST"])
 def registerSponsor():
     if request.method == "POST":
-        pass
+
+        role = "sponsor"
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+        if password != confirm_password:
+            return render_template('register.html', errMessage = "Passwords do not match!", emailInput = email)
+
+        with sqlite3.connect('users.db') as users:
+            cursor = users.cursor()
+
+            cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+            
+            if cursor.fetchall():
+                return render_template('register.html', errMessage = "User already exists!", emailInput=email)
+            else:
+                cursor.execute('INSERT INTO users(email, password, role) VALUES(?, ?, ?)', (email, password, role))
+                users.commit()
+
+                return cursor.execute("SELECT * FROM users").fetchall()
 
     return render_template("register.html", role="Sponsor")
 
 @app.route("/register/influencer", methods=["GET", "POST"])
 def registerInfluencer():
     if request.method == "POST":
-        pass
+        role = "influencer"
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+        if password != confirm_password:
+            return render_template('register.html', errMessage = "Passwords do not match!", emailInput = email)
+        
+        with sqlite3.connect('users.db') as users:
+            cursor = users.cursor()
+
+            cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+            
+            if cursor.fetchall():
+                return render_template('register.html', errMessage = "User already exists!", emailInput=email)
+            else:
+                cursor.execute('INSERT INTO users(email, password, role) VALUES(?, ?, ?)', (email, password, role))
+                users.commit()
+                return cursor.execute("SELECT * FROM users").fetchall()
 
     return render_template("register.html", role="Influencer")
 
