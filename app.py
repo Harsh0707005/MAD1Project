82280dad2b8787fff27aa3bab54ac56d6f515203
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-17 00:33:40 +0530

    Authentication complete

--- app.py
+++ app.py
@@ -4,60 +4,92 @@ import sqlite3
 app = Flask(__name__)
 
 connect = sqlite3.connect('users.db')
-connect.execute('CREATE TABLE IF NOT EXISTS users (email TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL, role TEXT NOT NULL)')
+connect.execute('CREATE TABLE IF NOT EXISTS users (username TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL, role TEXT NOT NULL)')
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        email = request.form['email'].strip()
+        username = request.form['username'].strip()
         password = request.form['password'].strip()
 
-        if email == "" or password == "":
-            return render_template('login.html', errMessage = "Please fill in all fields!", emailInput = email)
+        if username == "" or password == "":
+            return render_template('login.html', errMessage = "Please fill in all fields!", usernameInput = username)
 
         with sqlite3.connect('users.db') as users:
             cursor = users.cursor()
 
-            cursor.execute('SELECT * FROM users where email = ?', (email,))
+            cursor.execute('SELECT * FROM users where username = ?', (username,))
 
             db_res = cursor.fetchone()
 
             if db_res:
-                if password == db_res[1]:
-                    return render_template('login.html', errMessage = "Login Successful!")
+                if db_res[2] == "admin":
+                    return render_template('login.html', role="User" , errMessage = "Login as admin!")
+                elif password == db_res[1]:
+                    return render_template('login.html', role="User" , errMessage = "Login Successful!")
                 else:
-                    return render_template('login.html', emailInput = email, errMessage = "Invalid Password!")
+                    return render_template('login.html', role="User" , usernameInput = username, errMessage = "Invalid Password!")
                 
             else:
-                return render_template('login.html', errMessage = "User doesn't exist!")
+                return render_template('login.html', role="User" , errMessage = "User doesn't exist!")
 
-    return render_template("login.html")
+    return render_template("login.html", role="User")
+
+@app.route("/login/admin", methods=['GET', 'POST'])
+def admin():
+    if request.method == "POST":
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
+
+        if username == "" or password == "":
+            return render_template('login.html', role="Admin", errMessage = "Please fill in all fields!", usernameInput = username)
+
+        with sqlite3.connect('users.db') as users:
+            cursor = users.cursor()
+
+            cursor.execute('SELECT * FROM users where username = ?', (username,))
+
+            db_res = cursor.fetchone()
+
+            if db_res:
+                if db_res[2] == "admin":
+                    if password == db_res[1]:
+                        return render_template('login.html', role="Admin" , errMessage = "Login Successful!")
+                    else:
+                        return render_template('login.html', role="Admin" , usernameInput = username, errMessage = "Invalid Password!")
+                else:
+                    return render_template('login.html', role="Admin" , errMessage = "No Admin Users Found!")
+            else:
+                return render_template('login.html', role="Admin" , errMessage = "User doesn't exist!")
+
+    return render_template('login.html', role="Admin")
+
 
 @app.route("/register/sponsor", methods=["GET", "POST"])
 def registerSponsor():
     if request.method == "POST":
 
         role = "sponsor"
-        email = request.form["email"].strip()
+        username = request.form["username"].strip()
         password = request.form["password"].strip()
         confirm_password = request.form["confirm_password"]
 
-        if email == "" or password == "":
-            return render_template('register.html', errMessage = "Please fill in all fields!", emailInput = email)
+        if username == "" or password == "":
+            return render_template('register.html', role="Sponsor" , errMessage = "Please fill in all fields!", usernameInput = username)
 
         if password != confirm_password:
-            return render_template('register.html', errMessage = "Passwords do not match!", emailInput = email)
+            return render_template('register.html', role="Sponsor" , errMessage = "Passwords do not match!", usernameInput = username)
 
         with sqlite3.connect('users.db') as users:
             cursor = users.cursor()
 
-            cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+            cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
             
             if cursor.fetchall():
-                return render_template('register.html', errMessage = "User already exists!", emailInput=email)
+                return render_template('register.html', role="Sponsor" , errMessage = "User already exists!", usernameInput=username)
             else:
-                cursor.execute('INSERT INTO users(email, password, role) VALUES(?, ?, ?)', (email, password, role))
+                cursor.execute('INSERT INTO users(username, password, role) VALUES(?, ?, ?)', (username, password, role))
                 users.commit()
 
                 return cursor.execute("SELECT * FROM users").fetchall()
@@ -68,25 +100,25 @@ def registerSponsor():
 def registerInfluencer():
     if request.method == "POST":
         role = "influencer"
-        email = request.form["email"].strip()
+        username = request.form["username"].strip()
         password = request.form["password"].strip()
         confirm_password = request.form["confirm_password"].strip()
 
-        if email == "" or password == "" or confirm_password == "":
-            return render_template('register.html', errMessage = "Please fill in all fields!", emailInput = email)
+        if username == "" or password == "" or confirm_password == "":
+            return render_template('register.html', role="Influencer" , errMessage = "Please fill in all fields!", usernameInput = username)
 
         if password != confirm_password:
-            return render_template('register.html', errMessage = "Passwords do not match!", emailInput = email)
+            return render_template('register.html', role="Influencer" , errMessage = "Passwords do not match!", usernameInput = username)
         
         with sqlite3.connect('users.db') as users:
             cursor = users.cursor()
 
-            cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+            cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
             
             if cursor.fetchall():
-                return render_template('register.html', errMessage = "User already exists!", emailInput=email)
+                return render_template('register.html', role="Influencer" , errMessage = "User already exists!", usernameInput=username)
             else:
-                cursor.execute('INSERT INTO users(email, password, role) VALUES(?, ?, ?)', (email, password, role))
+                cursor.execute('INSERT INTO users(username, password, role) VALUES(?, ?, ?)', (username, password, role))
                 users.commit()
                 return cursor.execute("SELECT * FROM users").fetchall()
 
