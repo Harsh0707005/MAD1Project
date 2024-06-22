89f9e5377ab02e850ffdbfe18518857c61d6d3d4
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-22 15:56:15 +0530

    db request received sent

--- sqlite_manage.py
+++ sqlite_manage.py
@@ -9,7 +9,7 @@ cursor = connect.cursor()
 
 # cursor.execute("SELECT * FROM users")
 # cursor.execute("DROP TABLE campaigns")
-cursor.execute("SELECT * FROM influencers")
+# cursor.execute("SELECT * FROM influencers")
 # cursor.execute("DELETE FROM users WHERE username = 'abc' OR username = 'bcd'")
 # cursor.execute("INSERT INTO campaigns (title, description, image, niche, sponsor, budget, date) values (?, ?, ?, ?, ?, ?, ?)", ('tes', 'ta', 'im', 'ca', 'bcd', 0, 'tate'))
 # connect.commit()
@@ -19,5 +19,24 @@ cursor.execute("SELECT * FROM influencers")
 # cursor.execute('DELETE FROM users WHERE username =? ', ('test',))
 # cursor.execute('PRAGMA foreign_key_list(influencers);')
 # cursor.execute('INSERT INTO influencers (username, presence, profic_pic, requests, total_earnings, rating) VALUES(?, ?, ?, ?, ?, ?)', ('test2', "", "", "", 0, 0))
+
+
+def copy():
+    with sqlite3.connect('users.db') as conn1:
+        cursor = conn1.cursor()
+        conn1.execute('CREATE TABLE IF NOT EXISTS new_campaigns (id INTEGER NOT NULL PRIMARY KEY, title TEXT, description TEXT, image TEXT, niche TEXT, requests TEXT, influencer TEXT, sponsor TEXT, budget NUMERIC, date TEXT)')
+        conn1.commit()
+        cursor.execute('INSERT INTO new_campaigns(id, title, description, image, niche, influencer, sponsor, budget, date) SELECT id, title, description, image, niche, influencer, sponsor, budget, date FROM campaigns')
+        conn1.commit()
+        cursor.execute('SELECT * FROM new_campaigns')
+        print(cursor.fetchall())
+        cursor.execute('SELECT * FROM campaigns')
+        print(cursor.fetchall())
+
+# copy()
+# connect.execute('DROP TABLE campaigns')
+# connect.execute('ALTER TABLE new_campaigns RENAME TO campaigns')
+
 connect.commit()
+cursor.execute('SELECT * FROM campaigns')
 print(cursor.fetchall())
